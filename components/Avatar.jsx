dfbccffa4c7e2771090012287cdb44d1fdc0caa0
Hari Om <118040921+MeHariom@users.noreply.github.com>
2024-02-12 22:42:36 +0530

    Add files via upload

---
+++ components/Avatar.jsx
@@ -0,0 +1,18 @@
+import { px } from "framer-motion";
+import Image from "next/image";
+
+const Avatar = () => {
+  return (
+    <div className="hidden xl:flex xl:max-w-none pointer-events-none select-none">
+      {/* <Image
+        src="/"
+        alt="avatar"
+        width={100}
+        height={150}
+        className="translate-z-0 w-full h-full"
+      /> */}
+    </div>
+  );
+};
+
+export default Avatar;
